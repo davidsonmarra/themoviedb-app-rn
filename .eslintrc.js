8d3098482eae37b7ca24edc22d14579247514249
Davidson Marra <davidson.vieira@stone.com.br>
2024-06-28 22:43:50 -0300

    chore: add testing library

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,14 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  overrides: [
+    {
+      // Test files only
+      files: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
+      extends: ['plugin:testing-library/react'],
+    },
+  ],
+  rules: {
+    'testing-library/no-node-access': ['off', {allowContainerFirstChild: true}],
+  },
 };
--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,4 @@
 module.exports = {
   preset: 'react-native',
+  setupFilesAfterEnv: ['./src/configs/jest.setup.js'],
 };
